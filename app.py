0da0a6ef4df1e8ff66fd93b38326ed1200d8a68b
Daniel Silveira Ribeiro <85583995+smarticogit@users.noreply.github.com>
2026-03-17 20:17:33 -0300

    fix: config port to deploy

--- app.py
+++ app.py
@@ -20,4 +20,9 @@ with app.app_context():
 app.register_blueprint(main_bp)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
+# Para rodar localmente
+# if __name__ == '__main__':
+#     app.run(debug=True)
